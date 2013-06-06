7cebe88fb8dc202732d0f971198860c9146c500c
Niko Schwarz <niko.schwarz@gmail.com>
2013-06-06 16:05:31 +0200

    Seems to work for harmless input.
    Barely tested, but kind of works.

---
+++ src/github.com/nes1983/sset/sset.go
@@ -0,0 +1,148 @@
+package sset
+
+const (
+	RED   Color = true
+	BLACK Color = false
+)
+
+type Color bool
+
+type Node interface {
+	// -1, if z < nd, 0 if z == nd, 1 if z > nd.
+	Cmp(nd Node) int
+	Left() (nd Node, ok bool)
+	Right() (nd Node, ok bool)
+	SetLeft(Node)
+	SetRight(Node)
+	Color() Color
+	SetColor(Color)
+	// Update the value to that of nd. Don't change Left or Right.
+	SetValue(nd Node)
+}
+
+type SortedSet struct {
+	root *Node
+}
+
+func Search(set SortedSet, nd Node) (Node, bool) {
+	if set.root == nil {
+		return nil, false
+	}
+
+	x := *set.root
+	for {
+		var ok bool
+		cmp := x.Cmp(nd)
+		switch {
+		case cmp == 0:
+			return x, true
+		case cmp < 0:
+			x, ok = x.Left()
+		case cmp > 0:
+			x, ok = x.Right()
+		}
+		if !ok {
+			return nil, false
+		}
+	}
+}
+
+func Len(set SortedSet) int {
+	if set.root == nil {
+		return 0
+	}
+	return nodeLen(*set.root)
+}
+
+func nodeLen(h Node) (ret int) {
+	ret = 1
+	if l, ok := h.Left(); ok {
+		ret += nodeLen(l)
+	}
+	if r, ok := h.Right(); ok {
+		ret += nodeLen(r)
+	}
+	return
+}
+
+func Insert(set *SortedSet, nd Node) {
+	_, leftOk := nd.Left()
+	_, rightOk := nd.Right()
+	if leftOk || rightOk {
+		panic("Nodes to be inserted should be nude, but weren't.")
+	}
+	if set.root == nil {
+		set.root = &nd
+	} else {
+		i := insert(*set.root, nd)
+		set.root = &i // Must be on different lines: can't take pointer of ret val.
+	}
+	(*set.root).SetColor(BLACK)
+}
+
+func insert(h Node, in Node) Node {
+	l, okL := h.Left()
+	r, okR := h.Right()
+
+	if okL && okR && l.Color() == RED && r.Color() == RED {
+		colorFlip(h)
+	}
+	if cmp := in.Cmp(h); cmp == 0 {
+		h.SetValue(in)
+	} else if okL && cmp < 0 {
+		h.SetLeft(insert(l, in))
+	} else if okR {
+		h.SetRight(insert(r, in))
+	}
+
+	if okL && okR && r.Color() == RED && !l.Color() == RED {
+		h = rotateLeft(h)
+	}
+	if okL {
+		if ll, okLL := l.Left(); okLL && l.Color() == RED && ll.Color() == RED {
+			h = rotateRight(h)
+		}
+	}
+	return h
+}
+
+func rotateLeft(h Node) (x Node) {
+	var okX bool
+	x, okX = h.Right()
+	l, okL := x.Left()
+	if !okX || !okL {
+		panic("For rotation, children must be there")
+	}
+
+	h.SetRight(l)
+	x.SetLeft(h)
+	x.SetColor(h.Color())
+	h.SetColor(RED)
+	return
+}
+
+func rotateRight(h Node) (x Node) {
+	var okX bool
+	x, okX = h.Left()
+	r, okR := x.Right()
+	if !okX || !okR {
+		panic("For rotation, children must be there")
+	}
+
+	h.SetLeft(r)
+	x.SetRight(h)
+	x.SetColor(h.Color())
+	h.SetColor(RED)
+	return
+}
+
+func colorFlip(h Node) {
+	r, okR := h.Right()
+	l, okL := h.Left()
+	if !okL || !okR {
+		panic("For rotation, children must be there")
+	}
+	h.SetColor(!h.Color())
+	l.SetColor(!l.Color())
+	r.SetColor(!r.Color())
+}
---
+++ src/github.com/nes1983/sset/sset_test.go
@@ -0,0 +1,56 @@
+package sset
+
+import "testing"
+
+type intNode struct {
+	val         int
+	left, right *intNode
+	color       Color
+}
+
+// -1, if z < nd, 0 if z == nd, 1 if z > nd.
+func (z intNode) Cmp(nd Node) int {
+	v := nd.(*intNode).val
+	if z.val < v {
+		return -1
+	} else if z.val == v {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+func (z intNode) Left() (nd Node, ok bool)  { return z.left, z.left != nil }
+func (z intNode) Right() (nd Node, ok bool) { return z.right, z.right != nil }
+func (z *intNode) SetLeft(nd Node)          { (*z).left = nd.(*intNode) }
+func (z *intNode) SetRight(nd Node)         { (*z).right = nd.(*intNode) }
+func (z intNode) Color() Color              { return z.color }
+func (z *intNode) SetColor(cl Color)        { (*z).color = cl }
+func (z *intNode) SetValue(nd Node)         { z.val = nd.(*intNode).val }
+
+func HarmlessTest(t *testing.T) {
+	var set SortedSet
+	_, found := Search(set, &intNode{val: 2})
+	if found {
+		t.Errorf("expecting to not find 2, but did.")
+	}
+
+	Insert(&set, &intNode{val: 1})
+	if l := Len(set); l != 1 {
+		t.Errorf("expecting len 1, but got %d", l)
+	}
+	Insert(&set, &intNode{val: 1})
+	if l := Len(set); l != 1 {
+		t.Errorf("expecting len 1, but got %d", l)
+	}
+	actual, found := Search(set, &intNode{val: 1})
+	if !found {
+		t.Errorf("expecting to find key, but was none.")
+	} else if actual.(*intNode).val != 1 {
+		t.Errorf("Expecting to find key 1, but was %d", actual.(*intNode).val)
+	}
+	_, found = Search(set, &intNode{val: 2})
+	if found {
+		t.Errorf("expecting to not find 2, but did.")
+	}
+}
