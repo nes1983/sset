489a8456b681584c3494c64f45e5f3c211c54b3e
Niko Schwarz <niko.schwarz@gmail.com>
2013-06-07 21:53:25 +0200

    Added a set of tests from biogo.llrb.
    See http://code.google.com/p/biogo/source/browse/llrb_test.go
    
    Simplified the tests on the way. Who needs gocheck?

--- sset_test.go
+++ sset_test.go
@@ -48,16 +48,32 @@ func TestHarmless(t *testing.T) {
 	set.Insert(&intNode{val: 3})
 	set.Insert(&intNode{val: 4})
 	set.Insert(&intNode{val: 5})
-	
+
 	if l := set.Len(); l != 5 {
 		t.Errorf("Expecting len to be 5, but was %v", l)
-	} 
+	}
 }
 
 func BenchmarkInsert(b *testing.B) {
 	var t SortedSet
 	for i := 0; i < b.N; i++ {
 		t.Insert(&intNode{val: b.N - i})
+	}
+}
+
+func TestMakeAndDescribeTree(t *testing.T) {
+	expected := "();"
+	if actual := describeTree((*Node)(nil), false); actual != expected {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+	for _, desc := range []string{
+		"();",
+		"((a,c)b,(e,g)f)d;",
+	} {
+		if actual := describeTree(makeTree(desc), false); actual != desc {
+			t.Errorf("makeTree and describeTree should be symmetric, but weren't. "+
+				"Input desc %v produced %v", desc, actual)
+		}
 	}
 }
 
@@ -76,12 +92,53 @@ func BenchmarkGet(b *testing.B) {
 	}
 }
 
+// Build a tree from a simplified Newick format returning the root node.
+// Single letter node names only, no error checking and all nodes are full or leaf.
+func makeTree(desc string) *Node {
+	if desc == "();" {
+		return nil
+	}
+	buf := bytes.NewBufferString(desc)
+	var build func() Node // Cannot be defined via ":=", because recursive.
+	build = func() Node {
+		if buf.Len() == 0 {
+			return nil
+		}
+
+		var cn intNode
+		for {
+			b, _, _ := buf.ReadRune()
+			switch b {
+			case '(':
+				cn.GetNodeInfo().left = build()
+			case ',':
+				cn.GetNodeInfo().right = build()
+			case ')': // Ignore
+			default:
+				if b != ';' {
+					cn.val = int(b)
+				}
+				return &cn
+			}
+		}
+
+		panic("Unreachable")
+	}
+
+	n := build()
+	if n.GetNodeInfo().left == nil && n.GetNodeInfo().right == nil {
+		n = nil
+	}
+
+	return &n
+}
+
 // Return a Newick format description of a tree defined by a node.
-func describeTree(n *Node) string {
+func describeTree(n *Node, color bool) string {
 	if n == nil {
-		return "()"
-	} 
-	
+		return "();"
+	}
+
 	var s bytes.Buffer
 
 	var follow func(*Node)
@@ -103,13 +160,81 @@ func describeTree(n *Node) string {
 		if l != nil || r != nil {
 			s.WriteString(")")
 		}
-		col := func (c color) string { if c == red { return "r" }; return "b"}
-		s.WriteString(fmt.Sprintf("%d %v", (*n).(*intNode).val, col((*n).GetNodeInfo().color)))
+
+		s.WriteString(fmt.Sprintf("%c", rune((*n).(*intNode).val)))
+		if color {
+			if (*n).GetNodeInfo().color == red {
+				s.WriteRune('r')
+			} else {
+				s.WriteRune('b')
+			}
+		}
 	}
-	
 	follow(n)
-	
+
 	s.WriteString(";")
 
 	return s.String()
+}
+
+// Do all paths from root to leaf have same number of black edges?
+func (t SortedSet) isBalanced() bool {
+	if t.root == nil {
+		return true
+	}
+	var nblack int // number of black links on path from root to min
+	for x := t.root; x != nil; x = x.GetNodeInfo().left {
+		l := x.GetNodeInfo().left
+		if l != nil && l.GetNodeInfo().color == black {
+			nblack++
+		}
+	}
+	return isBalance(t.root.GetNodeInfo().left, nblack) &&
+		isBalance(t.root.GetNodeInfo().right, nblack)
+}
+
+// Does every path from the root to a leaf have the given number
+// of black links?
+func isBalance(n Node, nblack int) bool {
+	if n == nil && nblack == 0 {
+		return true
+	} else if n == nil && nblack != 0 {
+		return false
+	}
+	if n.GetNodeInfo().color == black {
+		nblack--
+	}
+	return isBalance(n.GetNodeInfo().left, nblack) &&
+		isBalance(n.GetNodeInfo().right, nblack)
+}
+
+func TestRotateLeft(t *testing.T) {
+	// Not sure how wise it is to test private methods ...
+	spec := "((a,c)b,(e,g)f)d;"
+	wanted := "(((a,c)b,e)d,g)f;"
+
+	tree := *makeTree(spec)
+
+	tree = rotateLeft(tree, tree.GetNodeInfo(), tree.GetNodeInfo().right.GetNodeInfo())
+	if actual := describeTree(&tree, false); actual != wanted {
+		t.Errorf("After rotation, wanted %v, but got %v.", actual, wanted)
+	}
+}
+
+func TestInsertion(t *testing.T) {
+	min, max := 1, 1000
+	var set SortedSet
+	for i := min; i <= max; i++ {
+		set.Insert(&intNode{val: i})
+		if actual := set.Len(); actual != i {
+			t.Errorf("Length should be %v, but was %v", i, actual)
+		}
+		if !set.isBalanced() {
+			t.Errorf("Length: %v", set.Len())
+			t.Fatalf("Tree should be balanced, but wasn't. Tree: %v", describeTree(&set.root, true))
+		}
+	}
+	// TODO:
+	//		failed = failed || !c.Check(t.isBST(), check.Equals, true)
+	//		failed = failed || !c.Check(t.is23_234(), check.Equals, true)
 }
